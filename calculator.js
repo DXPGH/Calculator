fcd3cb07a402b15c679c46be61acb137bc9b624b
DXPGH <dxpbusiness@gmail.com>
2022-07-30 14:00:20 -0500

    Applied Styling to Calc

---
+++ calculator.js
@@ -0,0 +1,28 @@
+const buttons = document.querySelectorAll("button");
+
+
+
+
+let a = 0;
+let b = 0;
+let result = 0;
+
+function add(a, b) {
+    result = a + b;
+    return result;
+}
+
+function subtract(a,b) {
+    result = a - b;
+    return result;
+}
+
+function multiply(a, b) {
+    result = a * b;
+    return result;
+}
+
+function divide(a, b) {
+    result = a / b;
+    return result;
+}
